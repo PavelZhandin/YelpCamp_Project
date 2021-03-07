fcf3148d05ea67bb677d6087cc1508ed5c2f0743
Zhandin Pavel <pavelzhandin@gmail.com>
2021-03-07 13:56:26 +0300

    Second commit modified different files. added Schemas  to

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const Joi = require('joi')
+const {campgroundSchema} = require('./schemas.js')
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
@@ -28,6 +30,16 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req,res,next)=>{
+    const { error } = campgroundSchema.validate(req.body);
+        if(error){
+            const msg = error.details.map(el=> el.message).join(',');
+            throw new ExpressError(msg, 400);
+        } else{
+            next();
+        }        
+}
+
 
 
 app.get('/', (req, res)=>{
@@ -43,9 +55,9 @@ app.get('/campgrounds/new', (req,res)=>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', catchAsync (async (req, res, next)=>{
-    // if(!req.body.campground) throw new ExpressError('Invalid CampGround Data', 400);
-    const campground = new Campground(req.body);
+app.post('/campgrounds', validateCampground, catchAsync (async (req, res, next)=>{
+    
+    const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);  
 }))
@@ -62,9 +74,9 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req,res,next)=>{
     res.render('campgrounds/edit',{camp});
 }))
 
-app.put('/campgrounds/:id',catchAsync( async (req,res,next)=>{
+app.put('/campgrounds/:id', validateCampground, catchAsync( async (req,res,next)=>{
     const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, req.body);
+    const campground = await Campground.findByIdAndUpdate(id, req.body.campground);
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
