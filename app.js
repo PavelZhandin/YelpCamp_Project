80ecf89f9196df5499364b7c37db69dff92552fb
Zhandin Pavel <pavelzhandin@gmail.com>
2021-03-16 22:17:26 +0300

    Routes
    separated routs to different directories
    added public directory
    added reviews functionality

--- app.js
+++ app.js
@@ -5,15 +5,23 @@ const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync'); 
 const ExpressError = require('./utils/ExpressError');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground');
 const Joi = require('joi')
-const {campgroundSchema} = require('./schemas.js')
 
+
+
+
+
+
+
+
+const campgorunds = require('./routes/campgrounds')
+const reviews = require('./routes/reviews')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -23,68 +31,27 @@ db.once("open", ()=>{
 });
 
 const app = express();
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const validateCampground = (req,res,next)=>{
-    const { error } = campgroundSchema.validate(req.body);
-        if(error){
-            const msg = error.details.map(el=> el.message).join(',');
-            throw new ExpressError(msg, 400);
-        } else{
-            next();
-        }        
-}
 
+
+
+
+app.use("/campgrounds", campgorunds)
+app.use("/campgrounds/:id/reviews", reviews)
 
 
 app.get('/', (req, res)=>{
     res.render('home')
-})
+});
 
-app.get('/campgrounds', catchAsync( async (req, res)=>{
-    const campgrounds = await Campground.find();
-    res.render('campgrounds/index',{campgrounds});
-}))
 
-app.get('/campgrounds/new', (req,res)=>{
-    res.render('campgrounds/new');
-})
-
-app.post('/campgrounds', validateCampground, catchAsync (async (req, res, next)=>{
-    
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);  
-}))
-
-app.get('/campgrounds/:id', catchAsync(async (req,res)=>{  
-        const {id} = req.params;
-        const campground = await Campground.findById(id);
-        res.render('campgrounds/show', {campground});   
-}));
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req,res,next)=>{
-    const {id} = req.params;
-    const camp = await Campground.findById(id);
-    res.render('campgrounds/edit',{camp});
-}))
-
-app.put('/campgrounds/:id', validateCampground, catchAsync( async (req,res,next)=>{
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, req.body.campground);
-    res.redirect(`/campgrounds/${campground._id}`)
-}));
-
-app.delete('/campgrounds/:id', catchAsync( async (req,res,next)=>{
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-}))
 
 
 app.all('*', (req, res, next)=>{
--- models/campground.js
+++ models/campground.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-const CampgroundSchema = new mongoose.Schema({
+const Review = require('./review')
+const CampgroundSchema = new Schema({
     title: String,
     price: Number,
     description: String,
     location: String,
-    image: String
+    image: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 })
-mongoose.connect('')
+CampgroundSchema.post('findOneAndDelete', async function(doc){
+   if(doc){
+      await Review.remove({
+          _id: {
+              $in: doc.reviews
+          }
+      })
+   }
+})
 
 module.exports = mongoose.model('campground', CampgroundSchema);
